dcb596a040a0916da02071069a1f20b333efaaa5
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-07 09:57:34 +0100

    Update example.js

--- commands/example.js
+++ commands/example.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 module.exports.run = async (bot, message, args) => {
   //this is where the actual code for the command goes
   await message.delete()
-  return message.reply("Hi! I'm an example command").then(m => m.delete(10000))
+  return message.reply("Hi, Tunahan is een meisje").then(m => m.delete(10000))
 }
 //name this whatever the command name is.
 module.exports.help = {
