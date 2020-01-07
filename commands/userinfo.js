de4bfe3496c8da7d32600ba81b70f14f4377594b
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-07 14:47:08 +0100

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -24,7 +24,40 @@ module.exports.run = async (bot, message, args) => {
 function getUserInfo(user, bot){
 
   //hier uw code tunahan!!
-  bot.channels.get(config.logChannelId).send("Verander mij naar iets nuttigs")
+
+
+
+  const embed = new Discord.RichEmbed()
+    .setTitle("Info voor " + user.tag)
+    .setAuthor(user.tag, user.displayAvatarURL)
+    /*
+    * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+    */
+    .setColor(0x00AE86)
+    .setDescription("Tunahan is echt een meisje ojooooo")
+    .setFooter("Tunahan kan echt niks ojooo")
+    // .setImage("http://i.imgur.com/yVpymuV.png")
+    // .setThumbnail("http://i.imgur.com/p2qNFag.png")
+    // /*
+    // * Takes a Date object, defaults to current date.
+    // */
+    // .setTimestamp()
+    // .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+    // .addField("This is a field title, it can hold 256 characters",
+    //   "This is a field value, it can hold 1024 characters.")
+    // /*
+    // * Inline fields may not display as inline if the thumbnail and/or image is too big.
+    // */
+    // .addField("Inline Field", "They can also be inline.", true)
+    // /*
+    // * Blank field, useful to create some space.
+    // */
+    // .addBlankField(true)
+    // .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+ 
+    bot.channels.get(config.logChannelId).send(embed)
+
+
 
 }
 
