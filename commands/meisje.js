1d5373a9c715f769999d620ed9624ac01d3ea319
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-07 10:07:40 +0100

    Create meisje.js

---
+++ commands/meisje.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args) => {
+  //this is where the actual code for the command goes
+  return message.reply("Hi, Tunahan is een meisje")
+}
+//name this whatever the command name is.
+module.exports.help = {
+  name: "example"
+}
