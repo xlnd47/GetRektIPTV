e2929435a78126e20798292c90ec6c112a36322d
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-08 08:51:12 +0100

    giveaway command, package update
    added giveaway command, installed new npm package.

---
+++ commands/giveaway.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js')
+const giveaways = require("discord.js-giveaway")
+
+module.exports.run = async (bot, message, args) => {
+
+    if (!message.member.roles.has(config.devID)){
+        return message.reply("Don't try me bru").then(m => m.delete(10000))
+    }
+    if (args[0] == undefined){
+        return message.reply("Geef mij prijs bru").then(m => m.delete(10000))
+    }
+
+    giveaways.start(message.channel, {
+        time: ms(args[1]),
+        prize: args[0],
+        winnersCount: parseInt(args[2])
+    }).then((gData) => {
+        console.log(gData); // {...} (messageid, end date and more)
+    });
+
+
+
+}
+//name this whatever the command name is.
+module.exports.help = {
+    name: "giveaway"
+}
