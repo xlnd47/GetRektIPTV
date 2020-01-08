cd4e01b126eb8203e243ecbc2ae7ff128eb6d137
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-08 13:48:34 +0100

    Create eval.js

---
+++ commands/eval.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js')
+const config = require("../config.json")
+
+module.exports.run = async (bot, message, args) => {
+    if (!message.member.roles.has(config.devID)){
+        return message.reply("Don't try me bru").then(m => m.delete(10000))
+    }
+
+    let evaled;
+    try {
+      evaled = await eval(args.join(' '));
+      message.channel.send(inspect(evaled));
+      console.log(inspect(evaled));
+    }
+    catch (error) {
+      console.error(error);
+      bot.channels.get(config.logChannelId).send(error)
+      message.reply('there was an error during evaluation.');
+    }
+
+}
+//name this whatever the command name is.
+module.exports.help = {
+  name: "example"
+}
