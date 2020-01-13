9a42408e28e1c7fd5cc5586715c1beaf21b5efc4
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-13 20:12:19 +0100

    Update db.js

--- commands/db.js
+++ commands/db.js
@@ -60,17 +60,11 @@ async function allesOphalen(bot){
         if (!error && response.statusCode == 200) {
             var result = await JSON.parse(response.body).result
             var data = await result.data
-                
+            var sql = "INSERT INTO users(username, password, expiredAt) VALUES "
             for (var i = 0; i < data.length; i++){
-                users.push(
-                    {
-                        username: data[i].username,
-                        password: data[i].password,
-                        expiredAt:  data[i].expired_at
-                    }
-                )
+                sql += "('" + data[i].username + "',"+data[i].password+"',FROM_UNIXTIME("+ data[i].expired_at + ")),";
             } 
-            console.log(users);
+            console.log(sql);
 
 
         } else {
