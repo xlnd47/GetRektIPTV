6d4c2154d33cf9b43e513fe01b2083b81afb3ae9
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-13 18:22:30 +0100

    Create db.js

---
+++ commands/db.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const config = require("../config.json");
+const mysql = require('mysql');
+
+module.exports.run = async (bot, message, args) => {
+  //this is where the actual code for the command goes
+  await message.delete()
+
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: config.dbUser,
+    password: config.dbPassword
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+  return message.reply("Connected!").then(m => m.delete(10000))
+  });
+
+
+}
+//name this whatever the command name is.
+module.exports.help = {
+  name: "db",
+  description: "Database"
+
+}
