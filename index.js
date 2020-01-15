cf61467da9a692f968e32cdd35b26e73013e41da
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-15 08:08:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js")
 const config = require("./config.json")
 const bot = new Discord.Client();
 const fs = require("fs");
+const mysql = require('mysql');
+
 
 bot.commands = new Discord.Collection();
 if(config.token === "setmeplease") return console.log("Set your token up! Go to https://www.discordapp.com/developers and generate a token from a bot user.");
