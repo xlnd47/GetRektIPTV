67de72e81989d0298389093fc8d397b67d4cac06
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-30 09:55:17 +0100

    Update addtest.js

--- commands/addtest.js
+++ commands/addtest.js
@@ -67,6 +67,8 @@ async function makeTrialWithUsername(bot, username, message){
 
     request(options, callback);    
     async function callback(error, response, body) {
+        console.log(apitoken);
+        console.log(response);
         if (!error && response.statusCode == 200) {
             var result = await JSON.parse(body).result
             await sendEmbededUsername(bot, result, message);
